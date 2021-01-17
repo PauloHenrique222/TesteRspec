361cba18fb442e716af09bc1b9b36f447accbda6
paulohenrique222 <pauloti4321@gmail.com>
2021-01-17 16:05:53 -0300

    test with rspec

---
+++ test_rspec/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+
+  enum kind: [:knight, :wizard]
+
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
+
+  def title
+    "#{kind} #{nickname} ##{level}"
+  end
+  
+end 
+ 
